d4378b86d680c64cdc17ef62a475848e848b9865
debby <debbychiu@outlook.com>
2019-05-04 13:38:54 -0700

    added tree generation

--- js/game.js
+++ js/game.js
@@ -7,6 +7,9 @@ var userScore = 10;
 var dialogue = document.getElementById("dialogue");
 dialogue.innerHTML = "Score: " + userScore;
 
+
+//grass background image
+function grass() {
 //background image variable and load
 var grassBackground = new Image();
 grassBackground.src = "../assets/sprites/grassField1.png";
@@ -14,4 +17,20 @@ grassBackground.src = "../assets/sprites/grassField1.png";
 grassBackground.onload = function() {
 ctx.drawImage(grassBackground, 0, 0);
 }
+}
 
+grass();
+
+function tree() {
+	var tree = new Image();
+	tree.src = "../assets/sprites/tree.png";
+	tree.onload = function() {
+	for(let i = 0; i < 20; i++) {
+			var randX = Math.floor(Math.random() * 750);
+			var randY = Math.floor(Math.random() * 550);
+			ctx.drawImage(tree, randX, randY);
+	}
+	}
+}
+
+tree();
