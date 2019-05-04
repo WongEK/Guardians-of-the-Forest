7d41ae88e744e37e1ecf1af1a0ac01adc4ab19c3
debby <debbychiu@outlook.com>
2019-05-04 13:16:24 -0700

    added grassbackground and score

--- js/game.js
+++ js/game.js
@@ -1,4 +1,16 @@
+//startup variables
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
+
+var userScore = 10;
+//can be used to hold score 
 var dialogue = document.getElementById("dialogue");
-dialogue.innerHTML = "i am changed inside game.js!";
+dialogue.innerHTML = "Score: " + userScore;
+
+//background image variable and load
+var grassBackground = new Image();
+grassBackground.src = "../assets/sprites/grassField1.png";
+
+grassBackground.onload = function() {
+ctx.drawImage(grassBackground,0, 0);
+}
