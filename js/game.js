72cad9f4a077ae02189f0957e6bc4d44d82a8865
debby <debbychiu@outlook.com>
2019-05-04 12:55:46 -0700

    added a basic canvas on index.html and changed the javascript file

---
+++ js/game.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+var dialogue = document.getElementById("dialogue");
+dialogue.innerHTML = "i am changed inside game.js!";
