40c4ccf4c3a23561e0ccb9054424e1f84f3a194b
Aleksandra Sorokina <alexas30s@gmail.com>
2019-05-05 10:59:29 -0700

    It's working!!!!!!!!!!!!

--- js/start.js
+++ js/start.js
@@ -1,13 +1,15 @@
+/* Code related to start screen */
+// game
+//startup variables
+var canvas1 = document.getElementById("game");
+var ctx1 = canvas1.getContext("2d");
 
 //startup variables
 var canvas2 = document.getElementById("start");
-var canvas1 = document.getElementById("game");
+var canvas3 = document.getElementById("gameOver");
 document.getElementById("game").style.display = "none";
 var ctx2 = canvas2.getContext("2d");
-// var ctx = document.getContext("2d");
 var header = 'Guardians of the Forest';
-
-//background image variable and load
 var startBackground = new Image();
 startBackground.src = "../assets/background/startBackground.png";
 var loaded = false;
@@ -22,10 +24,11 @@ button.addEventListener("load", loadButton, false);
 var myImage = new Image();
 myImage.src = "../assets/background/fireLong.png";
 myImage.addEventListener("load", loadImage, false);
- 
 
 
 
+
+//Event listener for start screen
 ctx2.canvas.addEventListener('click', function(e){
     ctx2.translate(0, 0);
     var mouseX = e.clientX;
@@ -38,28 +41,17 @@ ctx2.canvas.addEventListener('click', function(e){
 
 });
 
-
+// changes canvases from start to game
 function swapCanvases(){
     canvas1.style.display ='inline-block';
     canvas2.style.display ='none';
     canvas1.style.margin = 'marg%';
-
   }
 
 
+//fire image and amimation
 function loadImage(e) {
     animate();
-  }
-
-function createText(){
-    ctx2.font ="20px 'Press Start 2P'";
-    ctx2.fillStyle = "white";
-    ctx2.fillText(header, canvas2.width/5, canvas2.height/6);
-}
-
-function loadButton(e){
-    ctx2.drawImage(button, 350, 180, 260, 150);
-
 }
 
 function animate() {
@@ -79,5 +71,186 @@ function animate() {
     requestAnimationFrame(animate);
     
 }
+//Guardians of the Gorest title on the screen
+function createText(){
+    ctx2.font ="20px 'Press Start 2P'";
+    ctx2.fillStyle = "white";
+    ctx2.fillText(header, canvas2.width/5, canvas2.height/6);
+}
+
+// start button on start screen
+function loadButton(e){
+    ctx2.drawImage(button, 350, 180, 260, 150);
+
+}
 
 
+
+/* Code related to game */
+
+
+//plays music, node.js doesn't run require on browser/client
+/**var player = require('play-sound')(opts = {})
+
+ player.play('assets/sounds/gameSounds.mp3', function (err) {
+   if (err) throw err;
+   console.log("Audio finished");
+ });
+*/
+//add evenlistener to check for clicking fires
+ctx1.canvas.addEventListener('mousedown', function(e) {
+	var loc = windowToCanvas(e.clientX, e.clientY);
+	fireExtinguish(loc.x, loc.y);
+});
+
+function windowToCanvas(x, y) {
+    var r = ctx1.canvas.getBoundingClientRect();
+    return { x: x - r.left * (ctx1.canvas.width  / r.width),
+             y: y - r.top  * (ctx1.canvas.height / r.height)};
+};
+
+//variable for userscore
+var userScore = 0;
+
+//Setup score
+function setupScore() {
+    ctx1.font = "42pt VT323";
+    ctx1.fillStyle = "white";
+    ctx1.fillText("Score: " + userScore, 10, 35);
+}
+
+//holds trees
+var treeArr = [];
+
+var constTreeArr = [];
+
+//holds all fires
+var fireArr = [];
+
+//tree image
+ var tree = new Image();
+	tree.src = "assets/sprites/tree.png";
+	
+var fire = new Image();
+	fire.src = "assets/sprites/fireAnimation.gif";
+
+
+function genTrees() {
+    
+    //onload of tree
+	tree.onload = function() {
+        //for loop to push trees to array
+	    for (let i = 0; i < 200; i++) {
+           var randX = Math.floor(Math.random() * 750);
+           var randY = Math.floor(Math.random() * 500 + 50);
+           treeArr.push({
+               x: randX,
+               y: randY
+           });
+        }
+        
+        //for loop to draw images
+        for (let i = 0; i < 200; i++) {
+            ctx1.drawImage(tree, treeArr[i].x, treeArr[i].y);
+        }
+        //call the setup score function
+        setupScore();
+    }
+    
+}
+
+//function to sort the trees
+function sortTrees() {    
+    treeArr.sort((a, b) => (a.y > b.y) ? 1 : -1);
+}
+
+function reGenTrees() {
+    //create tree image
+    var newTree = new Image();
+	newTree.src = "assets/sprites/tree.png";
+    
+    newTree.onload = function() {
+        for (let i = 0; i < 200; i++) {
+            ctx1.drawImage(newTree, treeArr[i].x, treeArr[i].y);
+        }
+        setupScore();
+    }
+    
+}
+
+//semi-random fire generation
+function fireGeneration() {
+	
+	
+	//var coin = Math.floor(Math.random() * 2); //0,1
+	
+	var coin = Math.floor(Math.random() * 11); //0-10 
+
+	fire.onload = function() {
+		for (let i = 0; i < treeArr.length; i++) {
+			if(coin < 3) { //1/3 chance of fires starting to appear
+			ctx1.drawImage(fire, treeArr[i].x, treeArr[i].y);
+				fireArr.push({
+				x: treeArr[i].x,
+				y: treeArr[i].y
+				});
+				//fireArr[i].addEventListener('click', fireExtinguish(), false);
+			}
+			coin = Math.floor(Math.random() * 11);
+		}
+	}
+}
+
+
+
+// Re-Draw fires
+function reGenFires() {
+    
+    var newFire = new Image();
+    newFire.src = "assets/sprites/fireAnimation.gif";
+    
+    newFire.onload = function() {
+        
+        for (let i = 0; i < fireArr.length; i++) {
+            //console.log(fireArr[i].x + " is the x");
+			 // console.log(fireArr[i].y + " is the y");
+             if (fireArr[i].x && fireArr[i].y) {
+                ctx1.drawImage(newFire, fireArr[i].x, fireArr[i].y);
+             }
+            
+        }
+        
+    }
+}
+
+
+//Extinguish the fire clicked
+function fireExtinguish(x, y) {
+	
+    // Use the identity matrix while clearing the canvas
+    ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+    
+	for (let i = 0; i < fireArr.length; i++) {
+		 
+	   // var x,y; //Click offsets, here I assume they already have the value
+        var posx = fireArr[i].x + 15;
+        var posy = fireArr[i].y + 20; //Position of the arrow, the values you used as .drawImage parameters
+        var endx = posx + fire.width -30;
+        var endy = posy + fire.height -10;
+
+        if( (x>posx && y>posy) && (x<endx && y<endy) ) {
+            userScore += 10;
+            fireArr[i].x = null;
+			fireArr[i].y = null;
+			ctx1.drawImage(tree, treeArr[i].x, treeArr[i].y);
+        }
+	}
+
+    reGenTrees();
+    reGenFires();
+    
+}
+
+/* ----- CALLING FUNCTIONS ----- */
+genTrees();
+fireGeneration();
