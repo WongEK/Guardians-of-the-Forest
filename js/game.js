786ebc063f768de3625e501a29e2c55b70184f9e
joshgeransky <jgeransky91@gmail.com>
2019-05-04 17:03:51 -0700

    clears canvas and increments score

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
 //add evenlistener to check for clicking fires
-canvas.addEventListener('click', fireExtinguish(), false);
+//canvas.addEventListener('click', fireExtinguish(), false);
 
 //variable for userscore
 var userScore = 10;
@@ -21,6 +21,8 @@ function setupScore() {
 //holds trees
 var treeArr = [];
 
+var constTreeArr = [];
+
 //holds all fires
 var fireArr = [];
 
@@ -33,7 +35,7 @@ function genTrees() {
     //onload of tree
 	tree.onload = function() {
         //for loop to push trees to array
-	    for(let i = 0; i < 200; i++) {
+	    for (let i = 0; i < 200; i++) {
            var randX = Math.floor(Math.random() * 750);
            var randY = Math.floor(Math.random() * 500 + 50);
            treeArr.push({
@@ -52,11 +54,30 @@ function genTrees() {
         //call the setup score function
         setupScore();
     }
+    
 }
 
 //function to sort the trees
 function sortTrees() {    
     treeArr.sort((a, b) => (a.y > b.y) ? 1 : -1);
+}
+
+function reGenTrees() {
+    
+    //create tree image
+    var newTree = new Image();
+	newTree.src = "../assets/sprites/tree.png";
+    
+    newTree.onload = function() {
+        
+        for (let i = 0; i < 200; i++) {
+        ctx.drawImage(newTree, treeArr[i].x, treeArr[i].y);
+        }
+        setupScore();
+    }
+    
+
+    
 }
 
 //semi-random fire generation
@@ -69,7 +90,7 @@ function fireGeneration() {
 	var coin = Math.floor(Math.random() * 11); //0-10 
 
 	fire.onload = function() {
-		for(let i = 0; i < treeArr.length; i++) {
+		for (let i = 0; i < treeArr.length; i++) {
 			if(coin < 3) { //1/3 chance of fires starting to appear
 			ctx.drawImage(fire, treeArr[i].x, treeArr[i].y);
 				fireArr.push({
@@ -82,10 +103,51 @@ function fireGeneration() {
 	}
 }
 
+
+// Re-Draw fires
+function reGenFires() {
+    
+    var newFire = new Image();
+    newFire.src = "../assets/sprites/fireAnimation.gif";
+    
+    newFire.onload = function() {
+        
+        for (let i = 0; i < fireArr.length; i++) {
+            
+            ctx.drawImage(newFire, fireArr[i].x, fireArr[i].y);
+            
+            
+        }
+        
+    }
+    
+    
+    
+}
+
+//Extinguish the fire clicked
 function fireExtinguish() {
-	
+    	
     userScore += 10;
-	
+    
+    // Save the current transformation canvas
+    ctx.save();
+    
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Clear the canvas
+    ctx.clearRect(0, 0, ctx.width, ctx.height);
+    
+    // Restore the previous transformation canvas
+    ctx.restore();
+    
+    reGenTrees();
+    reGenFires();
+    
+    
+    
 }
 
 
