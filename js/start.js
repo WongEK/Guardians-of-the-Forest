7cda25e8fceeb47fcf8450a555d1dc64e95a69a5
Aleksandra Sorokina <alexas30s@gmail.com>
2019-05-04 19:21:16 -0700

    created landing screen

--- js/start.js
+++ js/start.js
@@ -13,27 +13,20 @@ var frameWidth = 360;
 var frameHeight = 360;
 var totalFrames = 25;
 var currentFrame = 0;
- 
+var button = new Image();
+button.src = "../assets/background/button.png";
+button.addEventListener("load", loadButton, false);
 var myImage = new Image();
 myImage.src = "../assets/background/fireLong.png";
 myImage.addEventListener("load", loadImage, false);
  
-
-//function for onload screen
-startBackground.onload = function() {
-    
-    ctx.drawImage(startBackground, -30, -30);
-    loaded = true;
-    handleLoaded();
-
-}
 
 // function for text
 function handleLoaded(startBackground) {
         
     if(loaded){
         
-        ctx.font ="20px 'Press Start 2P'";
+        ctx.font ="40px 'Press Start 2P'";
         ctx.fillStyle = "white";
         ctx.fillText(header, canvas.width/5, canvas.height/6);
         loadImage();
@@ -56,20 +49,31 @@ function handleLoaded(startBackground) {
 //    ctx.restore();
 
 function loadImage(e) {
-    
-    
+
     animate();
 
   }
+function createText(){
+    ctx.font ="20px 'Press Start 2P'";
+    ctx.fillStyle = "white";
+    ctx.fillText(header, canvas.width/5, canvas.height/6);
+}
+
+function loadButton(e){
+    
+    ctx.drawImage(button, 350, 180, 260, 150);
+
+}
+
 
 function animate() {
     ctx.save();
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, ctx.width, ctx.height);
-    ctx.restore();
+    ctx.clearRect(25, 25, 360, 360);
+    createText();
+    loadButton();
     //draw each frame + place them in the middle
     ctx.drawImage(myImage, shift, 0, frameWidth, frameHeight,
-                        120, 25, frameWidth, frameHeight);
+                        25, 25, frameWidth, frameHeight);
     shift += frameWidth;
     if (currentFrame == totalFrames) {
         shift = 0;
@@ -85,7 +89,27 @@ function animate() {
     */
     
     
+}
+
+
+function fireExtinguish() {
+    	
+    userScore += 10;
     
+    // Save the current transformation canvas
+    ctx.save();
     
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Clear the canvas
+    ctx.clearRect(0, 0, ctx.width, ctx.height);
+    
+    // Restore the previous transformation canvas
+    ctx.restore();
+    
+    reGenTrees();
+    reGenFires();
     
 }
